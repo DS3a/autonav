a7e8ac40a0ffdb623719de7544e8a5701c545395
deepesh <deepesh.padala02@gmail.com>
2021-12-02 02:05:37 +0530

    changed library name

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['lane_detection_gazebo'],
+    packages=['lane_detection_lib'],
     package_dir={'': 'src'}
 )
 setup(**d)
