c2673a37ec585c51f6adf5ce326c1a0b6ef2e196
Deepesh Padala <deepesh.padala02@gmail.com>
2022-03-15 23:21:48 +0530

    added camerainfo publisher

--- scripts/lane_mask_publisher.py
+++ scripts/lane_mask_publisher.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
+import queue
 import rospy
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import Image, CameraInfo
 from std_msgs.msg import Float64, Bool
 from geometry_msgs.msg import Twist
 import lane_detection_lib
@@ -20,7 +21,15 @@ DEPTH_IMAGE_TOPIC_NAME = "/camera/depth/image_rect_raw"
 rospy.init_node("lane_follower_node", anonymous=False)
 
 img_pub = rospy.Publisher("/lanes", Image, queue_size=2)
+info_pub = rospy.Publisher("/filtered_points/camera_info", CameraInfo, queue_size=2)
 mask = None
+
+camera_info_message = None
+info_found = False
+def camera_info_recvd(data: CameraInfo):
+    global camera_info_message, info_found
+    info_found = True
+    camera_info_message = data
 
 def depth_img_recvd(data: Image):
     cv_image = bridge.imgmsg_to_cv2(data, desired_encoding="passthrough")
@@ -29,6 +38,7 @@ def depth_img_recvd(data: Image):
     image_message = bridge.cv2_to_imgmsg(img)
     image_message.header = data.header
     img_pub.publish(image_message)
+    info_pub.publish(camera_info_message)
 
 def img_recvd(data: Image):
     cv_image = bridge.imgmsg_to_cv2(data, desired_encoding="passthrough")
@@ -45,6 +55,9 @@ def img_recvd(data: Image):
         pass
 
 if __name__ == "__main__":
+    info_sub = rospy.Subscriber("/camera/depth/camera_info", CameraInfo, camera_info_recvd)
+    if info_found:
+        info_sub.unregister()
     rospy.Subscriber(IMAGE_TOPIC_NAME, Image, img_recvd)
     rospy.Subscriber(DEPTH_IMAGE_TOPIC_NAME, Image, depth_img_recvd)
     rospy.spin()    
